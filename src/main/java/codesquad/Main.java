81b603287ade6425825cfa7d2b93b42f69ccfb35
minnim1010 <pms1010123@daum.net>
2024-07-02 11:02:29 +0900

    feat: 멀티 스레딩 기반 요청 처리 기능 구현
    - 스레드 풀을 이용하여 요청을 멀티 스레드로 처리할 수 있는 기능 구현

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -7,11 +7,15 @@ import codesquad.http.MyHttpRequest;
 import codesquad.socket.ClientSocket;
 import codesquad.socket.ServerSocket;
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 public class Main {
+
+    private static final int REQUEST_THREADS = 10;
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
     private static final HttpParser httpParser = new HttpParser();
@@ -19,26 +23,35 @@ public class Main {
     private static final HttpProcessor httpProcessor = new HttpProcessor(httpResponseFormatter);
 
     public static void main(String[] args) {
+        ExecutorService threadPool = Executors.newFixedThreadPool(REQUEST_THREADS);
+
         try (ServerSocket serverSocket = new ServerSocket();) {
             while (true) {
-                try {
-                    ClientSocket clientSocket = serverSocket.acceptClient();
-                    logger.debug("Client connected");
+                ClientSocket clientSocket = serverSocket.acceptClient();
+                threadPool.submit(() -> {
+                    try {
+                        int port = clientSocket.getPort();
+                        logger.debug("Client connected: port %d", port);
 
-                    String requestStr = clientSocket.read();
-                    MyHttpRequest request = httpParser.parse(requestStr)
-                            .orElseThrow(() -> new IllegalArgumentException("Invalid request"));
+                        String requestStr = clientSocket.read();
+                        MyHttpRequest request = httpParser.parse(requestStr)
+                                .orElseThrow(() -> new IllegalArgumentException("Invalid request"));
 
-                    String response = httpProcessor.processRequest(request);
-                    clientSocket.write(response);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                        String response = httpProcessor.processRequest(request);
+                        clientSocket.write(response);
+                    } catch (IOException e) {
+                        logger.error(e.getMessage());
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        logger.error(e.getMessage());
+                        e.printStackTrace();
+                    }
+                });
             }
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            threadPool.shutdown();
         }
     }
 }
