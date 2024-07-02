bea7cc786f9dfa3371eaf860c82a02aad78d1d16
minnim1010 <pms1010123@daum.net>
2024-07-02 19:58:16 +0900

    test: HTTP Request 파싱 테스트 케이스 추가
    - 여러 헤더 필드를 가진 요청인 경우 파싱 테스트 케이스 추가

--- src/test/java/codesquad/http/HttpParserTest.java
+++ src/test/java/codesquad/http/HttpParserTest.java
@@ -19,7 +19,20 @@ class HttpParserTest {
     @Nested
     class HTTP_요청을_파싱한다 {
         @Test
-        void 헤더가_있는_요청인_경우() {
+        void 헤더를_가지지_않는_요청인_경우() {
+            String httpRequestStr = "GET /index.html HTTP/1.1\r\n\r\n";
+
+            Optional<MyHttpRequest> result = parser.parse(httpRequestStr);
+
+            assertTrue(result.isPresent());
+            assertEquals("GET", result.get().method());
+            assertEquals("/index.html", result.get().path());
+            assertEquals("HTTP/1.1", result.get().version().getVersion());
+            assertTrue(result.get().headers().isEmpty());
+        }
+
+        @Test
+        void 헤더를_가진_요청인_경우() {
             String httpRequestStr = "GET /index.html HTTP/1.1\r\nHost: www.example.com\r\n\r\n";
 
             Optional<MyHttpRequest> result = parser.parse(httpRequestStr);
@@ -32,8 +45,16 @@ class HttpParserTest {
         }
 
         @Test
-        void 헤더가_없는_요청인_경우() {
-            String httpRequestStr = "GET /index.html HTTP/1.1\r\n\r\n";
+        void 여러_값을_가지는_헤더를_가진_요청인_경우() {
+            String httpRequestStr = """
+                    GET /index.html HTTP/1.1\r
+                    Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r
+                    Accept-Language: en-US,en;q=0.5\r
+                    Accept-Encoding: gzip, deflate, br\r
+                    Connection: keep-alive\r
+                    Upgrade-Insecure-Requests: 1\r
+                    \r
+                    """;
 
             Optional<MyHttpRequest> result = parser.parse(httpRequestStr);
 
@@ -41,7 +62,12 @@ class HttpParserTest {
             assertEquals("GET", result.get().method());
             assertEquals("/index.html", result.get().path());
             assertEquals("HTTP/1.1", result.get().version().getVersion());
-            assertTrue(result.get().headers().isEmpty());
+            assertEquals("text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8",
+                    result.get().getHeader("Accept"));
+            assertEquals("en-US,en;q=0.5", result.get().getHeader("Accept-Language"));
+            assertEquals("gzip, deflate, br", result.get().getHeader("Accept-Encoding"));
+            assertEquals("keep-alive", result.get().getHeader("Connection"));
+            assertEquals("1", result.get().getHeader("Upgrade-Insecure-Requests"));
         }
 
         @Test
