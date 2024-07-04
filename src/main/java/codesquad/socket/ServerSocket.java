4e7b8657d57ff2df16c8402bef662f2c4cc7a669
minnim1010 <pms1010123@daum.net>
2024-07-04 13:26:58 +0900

    refactor: 서버 시작 메시지 출력 방식 수정
    메시지 출력 방식에 대한 유연성 및 일관성을 위해 메시지 출력 방식 수정
    
    - 기존: System.out.println을 이용한 표준 출력
    - 변경: logger를 이용한 출력

--- src/main/java/codesquad/socket/ServerSocket.java
+++ src/main/java/codesquad/socket/ServerSocket.java
@@ -3,16 +3,19 @@ package codesquad.socket;
 import java.io.Closeable;
 import java.io.IOException;
 import java.net.Socket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ServerSocket implements Closeable {
 
+    private static final Logger log = LoggerFactory.getLogger(ServerSocket.class);
     private static final int SERVER_PORT = 8080;
 
     private java.net.ServerSocket serverSocket;
 
     public ServerSocket() throws IOException {
         this.serverSocket = new java.net.ServerSocket(SERVER_PORT);
-        System.out.println("Listening for connection on port " + SERVER_PORT);
+        log.info("Listening for connection on port " + SERVER_PORT);
     }
 
     public ClientSocket acceptClient() throws IOException {
