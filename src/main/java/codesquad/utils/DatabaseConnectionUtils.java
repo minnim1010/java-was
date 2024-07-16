1d88000278618e30d778432f502ad965171783ff
minnim1010 <pms1010123@daum.net>
2024-07-16 12:14:33 +0900

    feat: 데이터베이스 커넥션 풀 설정
    - 성능 향상을 위해 기존 커넥션을 만들어서 사용하는 로직을 커넥션 풀을 사용하는 로직으로 수정함

--- src/main/java/codesquad/utils/DatabaseConnectionUtils.java
+++ src/main/java/codesquad/utils/DatabaseConnectionUtils.java
@@ -2,19 +2,26 @@ package codesquad.utils;
 
 import codesquad.config.WasConfiguration;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import org.h2.jdbcx.JdbcConnectionPool;
 
 public class DatabaseConnectionUtils {
+
+    private static final JdbcConnectionPool jdbcConnectionPool;
+
+    static {
+        WasConfiguration wasConfiguration = WasConfiguration.getInstance();
+        String url = wasConfiguration.getDatasourceUrl();
+        String user = wasConfiguration.getDatasourceUser();
+        String password = wasConfiguration.getDatasourcePassword();
+
+        jdbcConnectionPool = JdbcConnectionPool.create(url, user, password);
+    }
 
     private DatabaseConnectionUtils() {
     }
 
     public static Connection getConnection() throws SQLException {
-        String url = WasConfiguration.getInstance().getDatasourceUrl();
-        String user = WasConfiguration.getInstance().getDatasourceUser();
-        String password = WasConfiguration.getInstance().getDatasourcePassword();
-
-        return DriverManager.getConnection(url, user, password);
+        return jdbcConnectionPool.getConnection();
     }
 }
