a12304eb83b8c55ea883c3f7b9daf20b0864df26
minnim1010 <pms1010123@daum.net>
2024-07-18 00:11:34 +0900

    feat: 댓글 작성을 index.html에서 할 수 있도록 댓글 작성 기능 수정
    - 기존 댓글 작성 페이지 삭제
    - 기존 댓글 작성 페이지 요청 처리 기능 삭제
    - index.html의 각 게시글에 댓글 작성란 추가

--- src/main/java/codesquad/application/handler/CommentWriteHandler.java
+++ src/main/java/codesquad/application/handler/CommentWriteHandler.java
@@ -5,9 +5,6 @@ import codesquad.application.persistence.CommentRepository;
 import codesquad.http.handler.AbstractDynamicRequestHandler;
 import codesquad.http.message.HttpRequest;
 import codesquad.http.message.HttpResponse;
-import codesquad.http.property.HttpStatus;
-import codesquad.template.TemplateContext;
-import codesquad.template.TemplateEngine;
 
 public class CommentWriteHandler extends AbstractDynamicRequestHandler {
 
@@ -15,22 +12,6 @@ public class CommentWriteHandler extends AbstractDynamicRequestHandler {
 
     public CommentWriteHandler(CommentRepository commentRepository) {
         this.commentRepository = commentRepository;
-    }
-
-    @Override
-    public void processGet(HttpRequest httpRequest, HttpResponse httpResponse) {
-        String userId = httpRequest.getSession().getAttribute("userId");
-        String articleId = httpRequest.getQuery("articleId");
-
-        TemplateContext templateContext = new TemplateContext();
-        templateContext.setValue("user", userId);
-        templateContext.setValue("articleId", articleId);
-
-        String renderedTemplate = TemplateEngine.getInstance().render("/comment_write.html", templateContext);
-
-        httpResponse.setBody(renderedTemplate.getBytes());
-        httpResponse.setHeader("Content-Type", "text/html");
-        httpResponse.setStatus(HttpStatus.OK);
     }
 
     @Override
