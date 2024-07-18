d4d2a4a1f3cf062fb17bdf4868bb01f032a48ef6
minnim1010 <pms1010123@daum.net>
2024-07-18 17:55:01 +0900

    fix: 글쓰기 시 입출력 예외 발생하는 버그 픽스
    - 이미지 저장 폴더를 초기화하지 않아 글쓰기 시 입출력 예외 발생하는 문제 발생
    - 초기에 이미지 저장 폴더가 없으면 만들어주는 로직 추가하여 해결함

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -1,6 +1,6 @@
 package codesquad;
 
-import codesquad.config.DatabaseInitializer;
+import codesquad.config.WasInitializer;
 import codesquad.context.WebContext;
 
 public class Main {
@@ -8,7 +8,8 @@ public class Main {
     public static void main(String[] args) {
         WebContext webContext = new WebContext();
         WebServer webServer = WebServerFactory.createWebServer(webContext);
-        DatabaseInitializer.initializeDatabase();
+        WasInitializer.init();
+
         webServer.run();
     }
 }
